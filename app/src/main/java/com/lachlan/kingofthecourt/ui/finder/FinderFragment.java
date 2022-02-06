d02deb4ec8bfe14c3969eed1f344c3f6d1c3e059
Lachlan Collins <lachlanjcollins@gmail.com>
2022-02-06 13:15:08 +1000

    removed mapbox stuff

--- app/src/main/java/com/lachlan/kingofthecourt/ui/finder/FinderFragment.java
+++ app/src/main/java/com/lachlan/kingofthecourt/ui/finder/FinderFragment.java
@@ -1,29 +1,28 @@
 package com.lachlan.kingofthecourt.ui.finder;
 
+
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.lachlan.kingofthecourt.R;
 import com.lachlan.kingofthecourt.databinding.FragmentFinderBinding;
-import com.mapbox.maps.MapView;
-import com.mapbox.maps.Style;
+
+
+import java.util.List;
 
 public class FinderFragment extends Fragment {
 
-    // @TODO: Update class to use bindings on layout elements?
-    // @TODO: Clean up class to only use necessary stuff.
-
     private FinderViewModel finderViewModel;
     private FragmentFinderBinding binding;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -34,24 +33,7 @@ public class FinderFragment extends Fragment {
         binding = FragmentFinderBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
 
-        String token = getString(R.string.mapbox_access_token);
-        MapView mapView = binding.mapView;
-        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
-
-        final TextView textView = binding.textFinder;
-        finderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return view;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 
 }

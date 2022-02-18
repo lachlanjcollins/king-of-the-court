62871b2e13b19bfca1c663d7279ce55ef625e562
Lachlan Collins <lachlanjcollins@gmail.com>
2022-02-19 09:21:31 +1000

    updated edit profile view

--- app/src/main/java/com/lachlan/kingofthecourt/fragments/ProfileFragment.java
+++ app/src/main/java/com/lachlan/kingofthecourt/fragments/ProfileFragment.java
@@ -1,21 +1,22 @@
 package com.lachlan.kingofthecourt.fragments;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.lachlan.kingofthecourt.R;
-import com.lachlan.kingofthecourt.ui.viewmodel.ProfileViewModel;
 import com.lachlan.kingofthecourt.ui.viewmodel.SharedViewModel;
 import com.lachlan.kingofthecourt.databinding.FragmentProfileBinding;
 import com.lachlan.kingofthecourt.activities.LoginActivity;
@@ -23,26 +24,32 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.lachlan.kingofthecourt.data.entity.User;
+import com.lachlan.kingofthecourt.util.Validation;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
     private SharedViewModel sharedViewModel;
     private FragmentProfileBinding binding;
+    private Validation valid;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         sharedViewModel = ViewModelProvider
                 .AndroidViewModelFactory
                 .getInstance(getActivity().getApplication())
                 .create(SharedViewModel.class);
 
-        NavController navController = NavHostFragment.findNavController(this);
+        valid = new Validation();
 
         final TextView textProfile = binding.textProfile;
+        final EditText editFName = binding.editFName;
+        final EditText editLName = binding.editLName;
+        final EditText editPosition = binding.editPosition;
+        final AppCompatButton buttonEditProfile = binding.buttonEditProfile;
+        final AppCompatButton buttonUpdate = binding.buttonUpdate;
+        final AppCompatButton buttonLogOut = binding.buttonLogOut;
 
         sharedViewModel.getUser().observe(getViewLifecycleOwner(), new Observer<User>() {
             @Override
@@ -51,7 +58,7 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        binding.logOutButton.setOnClickListener(new View.OnClickListener() {
+        buttonLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Sign out google client.
@@ -63,11 +70,68 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        binding.editProfileButton.setOnClickListener(new View.OnClickListener() {
+        sharedViewModel.getUser().observe(getViewLifecycleOwner(), new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                editFName.setText(user.getFirstName());
+                editLName.setText(user.getLastName());
+                editPosition.setText(user.getPosition());
+            }
+        });
+
+        buttonEditProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 binding.buttonUpdate.setVisibility(View.VISIBLE);
+                view.setBackgroundResource(R.drawable.bg_button_grey);
+                view.setClickable(false);
+                editFName.setTextColor(Color.BLACK);
+                editLName.setTextColor(Color.BLACK);
+                editPosition.setTextColor(Color.BLACK);
+                editFName.setFocusable(true);
+                editFName.setFocusableInTouchMode(true);
+                editFName.setClickable(true);
+                editLName.setFocusable(true);
+                editLName.setFocusableInTouchMode(true);
+                editLName.setClickable(true);
+                editPosition.setFocusable(true);
+                editPosition.setClickable(true);
+                editPosition.setFocusableInTouchMode(true);
+            }
+        });
 
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fName = editFName.getText().toString();
+                String lName = editLName.getText().toString();
+                String position = editPosition.getText().toString();
+
+                if (valid.isBlank(fName) || valid.isBlank(lName) || valid.isBlank(position)) {
+                    Toast.makeText(getContext(), "Please enter valid details", Toast.LENGTH_SHORT);
+                } else {
+                    view.setVisibility(View.INVISIBLE);
+                    buttonEditProfile.setBackgroundResource(R.drawable.bg_button_white);
+                    buttonEditProfile.setClickable(true);
+                    editFName.setTextColor(getResources().getColor(R.color.grey));
+                    editLName.setTextColor(getResources().getColor(R.color.grey));
+                    editPosition.setTextColor(getResources().getColor(R.color.grey));
+                    editFName.setFocusable(false);
+                    editFName.setFocusableInTouchMode(false);
+                    editFName.setClickable(false);
+                    editLName.setFocusable(false);
+                    editLName.setFocusableInTouchMode(false);
+                    editLName.setClickable(false);
+                    editPosition.setFocusable(false);
+                    editPosition.setClickable(false);
+                    editPosition.setFocusableInTouchMode(false);
+                    User user = sharedViewModel.getUser().getValue();
+                    user.setFirstName(fName);
+                    user.setLastName(lName);
+                    user.setPosition(position);
+                    sharedViewModel.updateUser(user);
+                    Toast.makeText(getContext(), "Details updated", Toast.LENGTH_SHORT);
+                }
             }
         });
 
--- app/src/main/java/com/lachlan/kingofthecourt/ui/viewmodel/SharedViewModel.java
+++ app/src/main/java/com/lachlan/kingofthecourt/ui/viewmodel/SharedViewModel.java
@@ -4,16 +4,10 @@ import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-import com.lachlan.kingofthecourt.data.database.RemoteDB;
 import com.lachlan.kingofthecourt.data.entity.User;
 import com.lachlan.kingofthecourt.data.repository.GameRepository;
 import com.lachlan.kingofthecourt.data.repository.UserRepository;
-import com.lachlan.kingofthecourt.fragments.EditProfileFragment;
-
-import java.util.List;
 
 public class SharedViewModel extends AndroidViewModel {
     private LiveData<User> currentUser;

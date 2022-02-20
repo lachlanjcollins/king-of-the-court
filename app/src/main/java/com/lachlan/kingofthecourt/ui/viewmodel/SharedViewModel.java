df6abb1e5d33f6a08f79aa061bee87055c16ddc8
Lachlan Collins <lachlanjcollins@gmail.com>
2022-02-20 10:03:14 +1000

    added string resources

--- app/src/main/java/com/lachlan/kingofthecourt/ui/viewmodel/SharedViewModel.java
+++ app/src/main/java/com/lachlan/kingofthecourt/ui/viewmodel/SharedViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import com.lachlan.kingofthecourt.data.entity.User;
+import com.lachlan.kingofthecourt.data.repository.CourtRepository;
 import com.lachlan.kingofthecourt.data.repository.GameRepository;
 import com.lachlan.kingofthecourt.data.repository.UserRepository;
 
@@ -13,13 +14,16 @@ public class SharedViewModel extends AndroidViewModel {
     private final LiveData<User> currentUser;
     private final UserRepository userRepository;
     private final GameRepository gameRepository;
+    private final CourtRepository courtRepository;
 
     public SharedViewModel(Application application) {
         super(application);
         userRepository = new UserRepository(application);
         gameRepository = new GameRepository(application);
+        courtRepository = new CourtRepository(application);
         currentUser = userRepository.getCurrentUser();
         gameRepository.getAllGames();
+        courtRepository.getAllCourts();
     }
 
     public LiveData<User> getUser() {
